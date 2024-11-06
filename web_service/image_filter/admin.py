c819eccc7e10b65b499bcd9e4f10dbe52475d652
DeOwl <deowl28@gmail.com>
2024-11-06 16:50:05 +0000

    admin added

--- web_service/image_filter/admin.py
+++ web_service/image_filter/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser, Filter, Queue, QueueFilter
 
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Filter)
+admin.site.register(Queue)
+admin.site.register(QueueFilter)
